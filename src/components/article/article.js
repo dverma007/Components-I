d87de0ef501aba698ffa749324d70d935fa42cfd
dverma007 <deepak@moresimple.us>
2022-01-12 19:25:28 -0700

    Wireframes are complete but not reading the database

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -94,6 +94,7 @@ const data = [
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
   and returns a DOM node looking like the one below:
+  
 
   <div class="article">
     <h2>{title of the article}</h2>
@@ -103,7 +104,42 @@ const data = [
 
     <span class="expandButton">+</span>
   </div>
+*/
+const articles = document.querySelector('.articles');
 
+  function articleMaker(artObj) {
+    const article = document.createElement('div')
+    const articleTitle = document.createElement('h2');
+    const articleDate = document.createElement('p');
+    const firstPgraph = document.createElement('p');
+    const secondPgraph = document.createElement('p');
+    const thirdPgraph = document.createElement('p');
+    const expandButton = document.createElement('span');
+
+    const {title, date, firstParagraph, secondParagraph, thirdParagraph} = data;
+
+    article.appendChild(articleTitle);
+    article.appendChild(articleDate);
+    article.appendChild(firstPgraph);
+    article.appendChild(secondPgraph);
+    article.appendChild(thirdPgraph);
+    article.appendChild(expandButton);
+
+    article.classList.add('article');
+    expandButton.classList.add('expandButton');
+
+    expandButton.addEventListener('click', evt => {
+      article.classList.toggle('article-open')
+    })
+    return article;
+  }
+  const articleElems = data.map(articleData => {
+    return articleMaker(articleData);
+  })
+  articleElems.forEach(elem => {
+    articles.appendChild(elem);
+  })
+  /*
   Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   This listener should toggle the class 'article-open' on div.article.
 
